49dfbd6ee35319094a99875d9512dc43c000bdd1
Soumya <sahukarsoumya6@gmail.com>
2026-04-02 18:53:23 +0530

    Time: 7 ms (65.73%) | Memory: 7.9 MB (21.12%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,16 +1,35 @@
+// class Solution {
+// public:
+//     int countTriples(int n) {
+//         int count = 0;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 for (int z = 1; z <= n; z++) {
+//                     if (i * i + j * j == z * z) {
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
 class Solution {
 public:
     int countTriples(int n) {
         int count = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int z = 1; z <= n; z++) {
-                    if (i * i + j * j == z * z) {
-                        count++;
-                    }
+
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c2 = a * a + b * b;
+                int c = sqrt(c2);
+
+                if (c <= n && c * c == c2) {
+                    count++;
                 }
             }
         }
+
         return count;
     }
 };

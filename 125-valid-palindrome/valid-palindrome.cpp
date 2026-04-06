040528656a27b32bbda4d6ef177c561fdfa2a83d
Soumya <sahukarsoumya6@gmail.com>
2026-04-06 18:34:08 +0530

    Time: 4 ms (15.66%) | Memory: 10.3 MB (15.14%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string r="";
+        for(int i=0;i<s.size();i++){
+	    if(s[i]>='a' && s[i]<='z'){
+	        r+=s[i];
+	    }else if(s[i]>='A'&&s[i]<='Z'){
+	        r+=('a'+(s[i]-'A'));
+	    }else if(s[i]>='0' &&s[i]<='9'){
+	        r+=s[i];
+	    }
+	}
+	int i=0,j=r.size()-1;
+	while(i<j){
+	    if(r[i]==r[j]){
+	        i++;
+	        j--;
+	    }
+	    else{
+	        return false;
+	    }
+	}
+	return true;
+        
+        
+    }
+};

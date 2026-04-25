dfbcae4c31f4e994ffc436d4a877325a69a2f7a5
Soumya <sahukarsoumya6@gmail.com>
2026-04-25 11:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (43.06%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        int primes[]={2,3,5};
+        for(int p:primes){
+            while(n%p==0){
+                n/=p;
+            }
+        }
+        return n==1;
+    }
+};

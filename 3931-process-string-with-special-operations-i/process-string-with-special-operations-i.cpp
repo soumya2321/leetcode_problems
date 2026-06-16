d1967a120b25ae2a77ae3de4b098832d16078994
Soumya <sahukarsoumya6@gmail.com>
2026-06-16 11:23:53 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+
+        for (char ch : s) {
+            if (islower(ch)) {
+                result += ch;
+            }
+            else if (ch == '*') {
+                if (!result.empty())
+                    result.pop_back();
+            }
+            else if (ch == '#') {
+                result += result;
+            }
+            else if (ch == '%') {
+                reverse(result.begin(), result.end());
+            }
+        }
+
+        return result;
+    }
+};

4733337d49b1bb8feb4b7caf48bf8d9f48c587bb
Soumya <sahukarsoumya6@gmail.com>
2026-07-01 18:46:36 +0530

    Time: 3 ms (9.22%) | Memory: 8.9 MB (37.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,52 +1,22 @@
-/*
 class Solution {
 public:
     bool isValid(string s) {
-        
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(' and s[i]=='[' and s[i]=='{'){
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
                 st.push(s[i]);
-            }
-            else{
-                if(st.empty()) return false;
-                if((s[i]==')' and st.top()=='(' or 
-                s[i]==']' and st.top()=='[' or 
-                s[i]=='}' and st.top()=='{')){
+            } else {
+                if (st.empty())
+                    return false;
+                if ((s[i] == '}' && st.top() == '{' ||
+                     s[i] == ']' && st.top() == '[' ||
+                     s[i] == ')' && st.top() == '(')) {
                     st.pop();
-                }
-                else{
+                } else {
                     return false;
                 }
             }
         }
-        return st.empty();    
-    }
-};
-
-*/
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('  or s[i]=='[' or s[i]=='{' ){
-                st.push(s[i]);
-            }
-            else{
-                if(st.empty()) return false;
-                if((s[i]==')' and st.top()=='(' 
-                or s[i]==']' and st.top()=='[' 
-                or s[i]=='}' and st.top()=='{') )
-                {
-                    st.pop();
-                }
-                
-                    else{
-                    return false;
-                }
-            }
-        }   
         return st.empty();
     }
 };

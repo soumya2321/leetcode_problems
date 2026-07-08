8f82f8772ec3578fd777fc2a28040a7a2798f72d
Soumya <sahukarsoumya6@gmail.com>
2026-07-08 18:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (99.42%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             int c=0;
             while(nums[i]!=0){
-                int rem=nums[i]%10;
+                //int rem=nums[i]%10;
                 c++;
                 nums[i]/=10;
             }

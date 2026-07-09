b263e3719b845f53987c88569efa3e0a1b382bee
Soumya <sahukarsoumya6@gmail.com>
2026-07-09 16:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (47.95%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -1,18 +1,18 @@
 class Solution {
-public:    
+public:
     bool canAliceWin(vector<int>& nums) {
-    int sum = 0;
-    int sum2 = 0;
-    int n=nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i]<10) 
-                sum = sum + nums[i];
-            else 
-                sum2 = sum2 + nums[i];
-    }
-    if (sum > sum2 || sum2 > sum) {
-        return true;
-    }
-    return false;
+        int sum1=0,sum2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=10){
+                sum2+=nums[i];
+            }
+            else{
+                sum1+=nums[i];
+            }
+        }
+        if(sum1>sum2 || sum2>sum1){
+            return true;
+        }
+        return false;
     }
 };

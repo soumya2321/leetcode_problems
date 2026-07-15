40b9d5f75b37a21529c6f51d7f724fac59222213
Soumya <sahukarsoumya6@gmail.com>
2026-07-15 18:50:40 +0530

    Time: 187 ms (5.75%) | Memory: 24 MB (98.80%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n=heights.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(heights[j]<heights[j+1]){
+                    swap(heights[j],heights[j+1]);
+                   swap(names[j],names[j+1]);
+                }
+            }
+        }
+        return names;
+    }
+};

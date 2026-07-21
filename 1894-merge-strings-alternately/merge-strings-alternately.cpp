d5cca6a6134c45fe4be1e99cbba1c32d6f62a8e3
Soumya <sahukarsoumya6@gmail.com>
2026-07-21 17:24:13 +0530

    Time: 3 ms (40.82%) | Memory: 8.4 MB (64.78%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res="";
+        int i=0;
+        int j=0;
+        while(i <word1.size() && j<word2.size()){
+            res.push_back(word1[i]);
+            res.push_back(word2[j]);
+            i++;
+            j++;
+        }
+        while(i<word1.size()){
+            res.push_back(word1[i]);
+            i++;
+        }
+        while(j<word2.size()){
+            res.push_back(word2[j]);
+            j++;
+        }
+        return res;
+    }
+};

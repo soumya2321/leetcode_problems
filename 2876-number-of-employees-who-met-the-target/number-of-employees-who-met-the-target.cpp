2f5c07d65c2f12ca6202d7b43f46d3a37a32c60c
Soumya <sahukarsoumya6@gmail.com>
2026-07-22 14:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (51.23%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
         int count=0;
-        for(int i=0;i<hours.size();i++){
-        if(hours[i]>=target){
-            count++;
-        }
+        for(int i = 0;i < hours.size(); i++ ){
+             if(hours[i] >= target )
+             count++;
         }
         return count;
     }

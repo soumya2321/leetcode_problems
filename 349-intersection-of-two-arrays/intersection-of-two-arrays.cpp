ff3cd1fab81d1149c12c682e4f60f18765d2f315
Soumya <sahukarsoumya6@gmail.com>
2026-07-23 15:04:49 +0530

    Time: 1 ms (48.23%) | Memory: 14.8 MB (7.73%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -10,18 +10,16 @@ public:
         for(int i=0;i<nums2.size();i++){
             s2.insert(nums2[i]);
         }
-        // for(int i=0;i<s1.size();i++){
-        //     for(int j=0;j<s2.size();j++){
-        //         if(s1[i]==s2[j]){
-        //             res.push_back(s1[i]);
+        // for(auto s :s1){
+        //     for(auto t:s2){
+        //         if(s==t){
+        //             res.push_back(s);
         //         }
         //     }
         // }
-        for(auto s :s1){
-            for(auto t:s2){
-                if(s==t){
-                    res.push_back(s);
-                }
+        for(auto a:s1){
+            if(s2.find(a)!=s2.end()){
+                res.push_back(a);
             }
         }
         return res;

4fa3b4813726160998aad4f3c322a67c79461634
Soumya <sahukarsoumya6@gmail.com>
2026-07-23 10:13:25 +0530

    Time: 1 ms (54.84%) | Memory: 9.2 MB (87.77%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string finalString(string s) {
+        int n=s.size();
+        string res="";
+        for(int i=0;i<n;i++){
+            if(s[i]=='i'){
+                reverse(res.begin(),res.end());
+            }
+            else{
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+        
+    }
+};

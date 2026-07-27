66c6e45e87988b2a74a63b3ea17b74a121ed616b
Soumya <sahukarsoumya6@gmail.com>
2026-07-27 14:00:42 +0530

    Time: 7 ms (81.36%) | Memory: 29.2 MB (25.14%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        int n=names.size();
+        vector<string>ans;
+        map<int,string,greater<int>>myMap;
         for(int i=0;i<names.size();i++){
-            for(int j=0;j<n-i-1;j++){
-                if(heights[j]<heights[j+1]){
-                    swap(heights[j],heights[j+1]);
-                    swap(names[j],names[j+1]);
-                }
-            }
+            myMap[heights[i]]=names[i];
         }
-        return names;
+        for(auto a:myMap){
+            ans.push_back(a.second);
+        }
+        return ans;
     }
 };

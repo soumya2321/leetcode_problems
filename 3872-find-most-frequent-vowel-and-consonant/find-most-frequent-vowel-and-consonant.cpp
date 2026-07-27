7303380c17960b7c55f7d5dc1f075135b2180af1
Soumya <sahukarsoumya6@gmail.com>
2026-07-27 14:15:29 +0530

    Time: 4 ms (15.62%) | Memory: 9.8 MB (40.69%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
+        int vc=0,cc=0;
         unordered_map<char,int>freq;
         for(int i=0;i<s.size();i++){
-             freq[s[i]]++;          
+            freq[s[i]]++;
         }
-        int maxx1=0;
-        int maxx2=0;
         for(auto a:freq){
             if(a.first=='a'||a.first=='e'||a.first=='i'||a.first=='o'||a.first=='u'){
-                maxx1=max(maxx1,a.second);
+                vc=max(vc,a.second);
             }
             else{
-                maxx2=max(maxx2,a.second);
+                cc=max(cc,a.second);
             }
         }
-        //cout<<maxx1<<" "<<maxx2<<endl;
-        return maxx2+maxx1;
-
-        
+        return cc+vc;
     }
 };

053414dd1494edf200063d3ecff2c107198a7776
Soumya <sahukarsoumya6@gmail.com>
2026-07-29 14:04:52 +0530

    Time: 1 ms (13.20%) | Memory: 9 MB (76.08%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        long x = 0,sum = 0,len = 1;
-        while(n != 0){
-            x = (n % 10) * len + x;
-            if(n % 10 != 0) len = len * 10;
-            sum += n % 10;
-            n = n / 10;
+        long long x=0,len=1,sum=0;
+        while(n!=0){
+            x=(n%10) *len+x;
+            if(n%10!=0) len=len*10;
+            sum+=n%10;
+            n/=10;
         }
-        return sum * x;
+        return sum*x;
     }
 };

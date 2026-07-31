8e4ec84e6a3c1a0c4b96c24ff2831823f9fc93cb
Soumya <sahukarsoumya6@gmail.com>
2026-07-31 07:10:37 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (41.19%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int>ans=nums;
+        vector<int>res=nums;
         for(int i=0;i<nums.size();i++){
-            ans.emplace_back(nums[i]);
+            res.push_back(nums[i]);
         }
-        return ans;
+        return res;
         
     }
 };

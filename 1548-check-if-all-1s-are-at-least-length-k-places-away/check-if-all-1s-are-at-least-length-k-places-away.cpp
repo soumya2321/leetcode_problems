9302506b7b2df8ff210e37287fdbcfdb497d0f0a
Soumya <sahukarsoumya6@gmail.com>
2026-08-03 07:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 61.6 MB (32.35%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int count=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                if(count != -1 && i - count - 1 < k){
+                    return false;
+                }
+                count=i;
+            }
+        }
+        return true;
+        
+    }
+};

753e3b3d1e891c94d7bdd32f7b26604c18525a9b
Soumya <sahukarsoumya6@gmail.com>
2026-08-27 15:11:46 +0530

    Time: 87 ms (78.09%) | Memory: 114.4 MB (78.26%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        unordered_set<int>res(candyType.begin(),candyType.end());
-        int n=candyType.size()/2;
+        // unordered_set<int>res(candyType.begin(),candyType.end());
+        int n=candyType.size();
+         unordered_set<int>res(n);
+         for(int i=0;i<n;i++){
+            res.insert(candyType[i]);
+         }
+        int z=candyType.size()/2;
         int m=res.size();
-        return min(n,m);
+        return min(z,m);
     }
 };
